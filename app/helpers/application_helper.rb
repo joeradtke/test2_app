aa56152e538fbd962b7bbfe1e98297782b82b36a
joeradtke <joeradtke@yahoo.com>
2020-06-03 18:58:00 -0600

    layout is done for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def here(name,path)
+    content_tag :li,class: current_page?(path) ? 'nav-item active' : 'nav-item' do 
+      link_to name,path,class: 'nav-link'
+    end
+  end
 end
