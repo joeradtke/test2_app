bd34c9fe8d81f3e3f6afa91eb8e017723b87ef98
joeradtke <joeradtke@yahoo.com>
2020-06-03 18:11:40 -0600

    index controller commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'index#index'
+  get 'about',to: 'index#about'
+  get 'contact',to: 'index#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
